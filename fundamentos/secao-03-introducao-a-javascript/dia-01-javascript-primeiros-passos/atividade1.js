db35c7a4f86b9116e66653d55fe0ef6abf882e18
Jeoflan <jjjeoflan@gmail.com>
2023-01-23 18:08:36 -0300

    atividade 1

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-javascript-primeiros-passos/atividade1.js
@@ -0,0 +1,9 @@
+const myName = "Jeoflan";
+const birthCity = "Aracaju";
+let birthYear = 1986;
+birthYear = 2030;
+birthCity = Aracaju;
+
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
