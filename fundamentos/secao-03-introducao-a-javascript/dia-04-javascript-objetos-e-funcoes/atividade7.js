08d00f2e92deda36cb66137b639474327bca5c52
Jeoflan <jjjeoflan@gmail.com>
2023-01-26 19:06:03 -0300

    atividade 7

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/atividade7.js
@@ -0,0 +1,9 @@
+function positiveNegative(number) {
+    if (number > 0) { 
+      return 'positive';
+    } else if (number < 0) { 
+      return 'negative';
+    } else { 
+      return 'zero';
+    }
+  }
